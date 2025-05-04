326c33d89b110ce28251b7df5aec788c8f63cd2a
Bipul Islam <bipulhstu@gmail.com>
2025-05-04 21:35:20 +0600

    learning user registration and login

--- src/main/kotlin/com/bipul/note_taking_app/security/AuthService.kt
+++ src/main/kotlin/com/bipul/note_taking_app/security/AuthService.kt
@@ -2,6 +2,7 @@ package com.bipul.note_taking_app.security
 
 import com.bipul.note_taking_app.database.model.User
 import com.bipul.note_taking_app.database.repository.UserRepository
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.stereotype.Service
 
 
@@ -12,12 +13,34 @@ class AuthService (
     private val hashEncoder: HashEncoder
 ){
 
+    data class TokenPair(
+        val accessToken: String,
+        val refreshToken: String
+    )
+
     fun register(email: String, password: String) : User {
         return userRepository.save(
             User(
                 email = email,
                 hashedPassword = hashEncoder.encode(password)
             )
+        )
+    }
+
+    fun login(email: String, password: String) : TokenPair{
+        val user = userRepository.findByEmail(email)
+            ?: throw BadCredentialsException("Invalid Credentials.")
+
+        if(!hashEncoder.matches(password, user.hashedPassword)) {
+            throw BadCredentialsException("Invalid Credentials.")
+        }
+
+        val newAccessToken = jwtService.generateAccessToken(user.id.toString())
+        val newRefreshToken = jwtService.generateRefreshToken(user.id.toString())
+
+        return TokenPair(
+            accessToken = newAccessToken,
+            refreshToken = newRefreshToken
         )
     }
 }
